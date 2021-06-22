fc8b988048518b96702260496ff85833547de5e9
leejuhyeon <esloven3@gmail.com>
2021-06-22 23:55:07 +0900

    Update

--- Weather.js
+++ Weather.js
@@ -1,28 +1,55 @@
 import React from "react";
 import propTypes from "prop-types";
-import { View, Text, StyleSheet } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
+import { View, Text, StyleSheet, StatusBar } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default class Weather extends React.Component {
   render() {
     var { temp } = this.props;
+    var { condition } = this.props;
 
     return (
-      <View style={styles.container}>
+      <LinearGradient
+        // Background Linear Gradient
+        colors={["#4c669f", "#3b5998", "#192f6a"]}
+        style={styles.container}
+      >
+        <StatusBar barStyle="light-content" />
         <View style={styles.halfContainer}>
           <MaterialCommunityIcons
-            name="weather-rainy"
+            name={weatherOptions[condition].iconName}
             size={96}
-            color="black"
+            color="white"
           />
-          <Text style={styles.temperature}>홀리쉣 최영민 {temp}°</Text>
+          <Text style={styles.temperature} color="white">
+            What a cool! {temp}°
+          </Text>
         </View>
         <View style={styles.halfContainer}></View>
-      </View>
+      </LinearGradient>
     );
   }
 }
+
+var weatherOptions = {
+  Haze: { iconName: "dehaze", gradient: ["#8e9eab", "#eef2f3"] },
+  Thunderstorm: { iconName: "thunderstorm", gradient: ["#304352", "#d7d2cc"] },
+  Drizzle: { iconName: "dehaze", gradient: ["#304352", "#d7d2cc"] },
+  Rain: { iconName: "dehaze", gradient: ["#304352", "#d7d2cc"] },
+  Snow: { iconName: "dehaze", gradient: ["#304352", "#d7d2cc"] },
+  Atmosphere: { iconName: "dehaze", gradient: ["#304352", "#d7d2cc"] },
+  Clear: { iconName: "dehaze", gradient: ["#304352", "#d7d2cc"] },
+  Clouds: { iconName: "dehaze", gradient: ["#304352", "#d7d2cc"] },
+  Mist: { iconName: "dehaze", gradient: ["#304352", "#d7d2cc"] },
+  Dust: { iconName: "dehaze", gradient: ["#304352", "#d7d2cc"] },
+  Fog: { iconName: "dehaze", gradient: ["#304352", "#d7d2cc"] },
+  Smoke: { iconName: "dehaze", gradient: ["#304352", "#d7d2cc"] },
+  Sand: { iconName: "dehaze", gradient: ["#304352", "#d7d2cc"] },
+  Ash: { iconName: "dehaze", gradient: ["#304352", "#d7d2cc"] },
+  Squall: { iconName: "dehaze", gradient: ["#304352", "#d7d2cc"] },
+  Tornado: { iconName: "dehaze", gradient: ["#304352", "#d7d2cc"] },
+};
 
 Weather.propTypes = {
   temp: propTypes.number.isRequired,
@@ -34,7 +61,6 @@ Weather.propTypes = {
     "Atmosphere",
     "Clear",
     "Clouds",
-    "Haze",
     "Mist",
     "Dust",
     "Fog",
@@ -43,6 +69,7 @@ Weather.propTypes = {
     "Ash",
     "Squall",
     "Tornado",
+    "Haze",
   ]).isRequired,
 };
 
@@ -59,5 +86,6 @@ var styles = StyleSheet.create({
   },
   temperature: {
     fontSize: 28,
+    color: "white",
   },
 });
